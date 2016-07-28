544a9d4955455e1de9f52e690975f6e79c7f2284
ellis736 <ryanellis736@gmail.com>
2016-07-28 12:56:42 -0500

    Update admins.pp

--- site/users/manifests/admins.pp
+++ site/users/manifests/admins.pp
@@ -1,6 +1,9 @@
 class users::admins {
   users::managed_user {'jose':}
-  users::managed_user {'alice':}
+  users::managed_user {'alice':
+   group => 'admin'
+   }
+  
   users::managed_user {'chen':
     group => 'admin',
   }
