0c0f76f7bd0b8a6acb958b3716031f1beb879a57
ellis736 <ryanellis736@gmail.com>
2016-07-27 20:43:59 -0500

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -22,7 +22,7 @@ class nginx {
    ensure => file,
    owner => 'root',
    group => 'root',
-   mode => '0664'
+   mode => '0664',
    source => 'puppet:///modules/nginx/files/nginx.conf',
    require => Package['nginx],
    notify => Service['nginx'],
