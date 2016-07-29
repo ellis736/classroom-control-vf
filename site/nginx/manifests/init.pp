d31a27b60e77217e13f164cf02929cd8f7892f9f
ellis736 <ryanellis736@gmail.com>
2016-07-29 11:01:56 -0500

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,47 +1,13 @@
 class nginx(
-$root = undef,
-){
-  case $::osfamily {
-  'redhat': {
-   $package = 'nginx'
-   $owner = 'root'
-   $group = 'root'
-   #$docroot = '/var/www'
-   $default_docroot ='/var/www'
-   $confdir = '/etc/nginx'
-   $logdir = '/var/log/nginx'
-   $user= 'nginx'
-   }
-   'debian': {
-   $package = 'nginx'
-   $owner = 'root'
-   $group = 'root'
-   #$docroot = '/var/www'
-   $default_docroot ='/var/www'
-   $confdir = '/etc/nginx'
-   $logdir = '/var/log/nginx'
-   $user= 'www-data'
-   }
-  'windows': {
-  $package = 'nginx-service'
-  $owner = 'Administrator'
-  $group = 'Administrators'
-  #$docroot = 'C:/ProgramData/nginx/html'
-  $default_docroot = 'C:/ProgramData/nginx/html'
-  $confdir = 'C:/ProgramData/nginx'
-  $logdir = ' C:/ProgramData/nginx/log'
-  $user = 'nobody'
-  }
-  default : {
-fail("Module ${module_name} is not supported on ${::osfamily}")
-  }
-}
-  # If $root isn't set, then fall back to the platform default
-  $docroot = $root ? {
-  undef => $default_docroot,
-  default => $root,
-  }
-  
+ $package::$nginx::params::package,
+ $owner::$nginx::params::owner,
+ $group::$nginx::params::group,
+ $default_docroot::$nginx::params::default_docroot,
+ $confdir::$nginx::params::confdir,
+ $logdir::$nginx::params::logdir,
+ $user::$nginx::params::user
+ ) inherits nginx::params {
+ 
   File {
    owner => $owner,
    group => $group,
